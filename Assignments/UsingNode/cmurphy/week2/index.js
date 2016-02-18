a8a5f555e31af7d0add192d3e5a662ab106eba21
Casey Murphy <st01k@lvaas.net>
2016-02-18 09:07:58 -0500

    mongodb integration

--- Assignments/UsingNode/cmurphy/week2/index.js
+++ Assignments/UsingNode/cmurphy/week2/index.js
@@ -1,7 +1,12 @@
 // dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+
+// 
 var app = express();
+var MongoClient = mongodb.MongoClient;
+var _db;
 
 // every route to parse body if available
 app.use(bodyParser.json());
@@ -11,153 +16,6 @@ app.get('/', function (req, res) {
 	res.sendFile("./index.html", {root : __dirname});
 });
 //------------------------------------------------------------------------------
-// data
-var shows = [
-	{
-		id: 1,
-		name: 'Stargate',
-		genre: 'Scifi',
-		spins: [
-		{
-			sName: 'SG1',
-			seasons: 10,
-			epPerSeason: [21, 22, 22, 22, 22, 22, 22, 20, 20, 20]
-		}, 
-		{
-			sName: 'Atlantis',
-			seasons: 5,
-			epPerSeason: [20, 20, 20, 20, 20]
-		},
-		{
-			sName: 'Universe',
-			seasons: 2,
-			epPerSeason: [20, 20]
-		}]
-	},
-	{
-		id: 2,
-		name: 'Downton Abbey',
-		genre: 'Drama',
-		spins: [
-		{
-			sName: 'Downton Abbey',
-			seasons: 6,
-			epPerSeason: [7, 9, 9, 9, 9, 9]
-		}]
-	},
-	{
-		id: 3,
-		name: 'Avatar',
-		genre: 'Animated',
-		spins: [
-		{
-			sName: 'The Last Airbender', 
-			seasons: 3,
-			epPerSeason: [16, 19, 17]
-		},
-		{
-			sName: 'Legend of Korra',
-			seasons: 4,
-			epPerSeason: [10, 14, 12, 12]
-		}]
-	},
-	{
-		id: 4,
-		name: 'Game of Thrones',
-		genre: 'Fantasy',
-		spins: [
-		{
-			sName: 'Game of Thrones',
-			seasons: 5,
-			epPerSeason: [10, 10, 10, 10, 10]
-		}]
-	},
-	{
-		id: 5,
-		name: 'Spartacus',
-		genre: 'Action',
-		spins: [
-		{
-			sName: 'Blood and Sand',
-			seasons: 1,
-			epPerSeason: [13]
-		},
-		{
-			sName: 'Gods of the Arena',
-			seasons: 1,
-			epPerSeason: [8]
-		},
-		{
-			sName: 'Vengeance',
-			seasons: 1,
-			epPerSeason: [10]
-		},
-		{
-			sName: 'War of the Damned',
-			seasons: 1,
-			epPerSeason: [10]
-		}]
-	},
-	{
-		id: 6,
-		name: 'Big Bang Theory',
-		genre: 'Sitcom',
-		spins: [
-		{
-			sName: 'Big Bang Theory',
-			seasons: 9,
-			epPerSeason: [17, 23, 23, 24, 24, 24, 24, 24, 24]
-		}]
-	},
-	{
-		id: 7,
-		name: 'Star Trek',
-		genre: 'Scifi',
-		spins: [
-		{
-			sName: 'The Original Series',
-			seasons: 3,
-			epPerSeason: [30, 26, 24]
-		},
-		{
-			sName: 'The Next Generation',
-			seasons: 7,
-			epPerSeason: [25, 22, 26, 26, 26, 26, 25]
-		},
-		{
-			sName: 'Deep Space Nine',
-			seasons: 7,
-			epPerSeason: [20, 26, 26, 26, 26, 26, 26]
-		},
-		{
-			sName: 'Voyager',
-			seasons: 7,
-			epPerSeason: [16, 26, 26, 26, 26, 26, 26]
-		},
-		{
-			sName: 'Enterprise',
-			seasons: 4,
-			epPerSeason: [26, 26, 24, 22]
-		},
-		{
-			sName: 'The Animated Series',
-			seasons: 2,
-			epPerSeason: [16, 6]
-		}]
-	},
-	{
-		id: 8,
-		name: 'Firefly',
-		genre: 'SciFi',
-		spins: [
-		{
-			sName: 'Firefly',
-			seasons: 1,
-			epPerSeason: [15]
-		}]
-	}
-];
-//------------------------------------------------------------------------------
 // serves server info
 app.get('/server', function (req, res) {
 	
@@ -166,112 +24,75 @@ app.get('/server', function (req, res) {
 
 	res.send(_hostname + ' (' + _ip + ') listening on port 3000');
 });
-//------------------------------------------------------------------------------
-// serves list
-app.get('/list', function (req, res) {
-	var partList = '';
-	var htmlList = '';
-	for (var i = 0; i < shows.length; i++) {
-		var _id = shows[i].id;
-		var _name = shows[i].name;
-		var _genre = shows[i].genre;
-		var _spins = shows[i].spins;
-		partList += _id + ': ' + 'Series: ' + _name + ', Genre: ' + _genre + '\n';
-		for (var j = 0; j < _spins.length; j++) {
-			var _seasons = _spins[j].seasons;
-			var _sName = shows[i].spins[j].sName;
-			var _episodes = shows[i].spins[j].epPerSeason;
-			var _episodeCnt = 0;
-			for (var k = 0; k < _episodes.length; k++)
-			{
-				_episodeCnt += _episodes[k];
-			}
-			var _info = _sName + ', Seasons: ' + _seasons + ', Total Episodes: ' + _episodeCnt;
-			partList += _info + '\n' + '<br/>';
-			//htmlList += '<li>' + _info + '</li>';
-		}
-		partList += '\n' + '<hr/>';
-		//htmlList += '<li>' + partList + '</li>';
-	}
-	//console.log(htmlList);
-	res.send(partList);
-	// res.render(htmlList, function(err, html) {
-	// 	html = "./list";
-	// });
-});
+
 //------------------------------------------------------------------------------
 // name and genre queries
 app.get('/shows', function (req, res) {
+	
+	var collection = _db.collection('tv_shows');
 
-	if (req.query.name) {
-		var qName = [];
-		for (var i = 0; i < shows.length; i++) {
-			if (shows[i].name.toLowerCase() == req.query.name.toLowerCase())
-				qName.push(shows[i]);
-		}
-		return res.send(qName);
-	}
+	collection.find({}, function(err, cursor) {
+		if(err)
+			return res.send(err);
 
-	if (req.query.genre) {
-		var qGenre = [];
-		for (var i = 0; i < shows.length; i++) {
-			if (shows[i].genre.toLowerCase() == req.query.genre.toLowerCase())
-				qGenre.push(shows[i]);
-		}
-		return res.send(qGenre);
-	}
-	res.send(shows);
+		cursor.toArray(function(err, docs) {
+			if(err)
+				return res.send(err);
+
+			res.send(docs);
+		});
+	});
+
 });
 //------------------------------------------------------------------------------
-// get show info with id
-app.get('/shows/:id', function (req, res) {
-	var _id = req.params.id;
-	var show;
-	for (var i = 0; i < shows.length; i++) {
-		if (_id == shows[i].id) {
-			show = shows[i];
-			break;
-		}
-	}
+// 
+app.get('/shows/:id', function(req, res) {
+	var collection = _db.collection('tv_shows');
+	var query = {
+		_id: mongodb.ObjectID(req.params.id)
+	};
 
-	if (!show)
-		return res.send('Entry does not exist.');
+	collection.findOne(query, function(err, doc) {
+		if(err)
+			return res.send(err);
 
-	res.send(show);
+		res.send(doc);
+	});
+
 });
 //------------------------------------------------------------------------------
-// update show name by id
-app.put('/shows/:id/:name', function (req, res) {
-	var _id = req.params.id;
-	var _name = req.params.name;
-	var _status = "id: " + _id + ", updated @ " + Date();
-	var show;
-	for (var i = 0; i < shows.length; i++) {
-		if (_id == shows[i].id) {
-			shows[i].name = _name;
-			show = shows[i];
-			break;
+// 
+app.get('/shows/genre/:genre', function(req, res) {
+	var collection = _db.collection('tv_shows');
+	var query = {
+		genre: {
+			$regex: req.params.genre,
+			$options: 'i'
 		}
+	};
+	
+	if (req.query.search) {
+		query.fanchise = {
+			$regex: '.*' + req.query.search + '.*',
+			$options: 'i'
+		}
+		console.log(JSON.stringify(query));
 	}
 
-	if (!show)
-		return res.send('Entry does not exist.  see /list for current entries.');
+	collection.find(query, function(err, cursor) {
+		if (err)
+			return res.send(err);
 
-	console.log(show);
-	res.send(_status);
-});
-//------------------------------------------------------------------------------
-// create show name, increments id
-// will break /list because all fields in 'shows' are not completed
-app.post('/shows/:name', function (req, res) {
-	var _name = req.body;
+		cursor
+			.limit(50)
+			.toArray(function(err, docs) {
+				if (err)
+					return res.send(err);
 
-	_name.id = shows.length + 1;
-	_name.name = req.params.name;
-	shows.push(_name);
-	var _status = "id: " + _name.id + ", created @ " + Date();
-	console.log(_name);
-	res.send(_status);
+				res.send(docs);
+			});
+	});
+
 });
 //------------------------------------------------------------------------------
 // deletes entry by id with splice
@@ -297,7 +118,18 @@ app.delete('/shows/:id', function (req, res) {
 
 });
 //------------------------------------------------------------------------------
+// connects to database
 // run server on PORT 3000, show confirmation in console
-app.listen(3000, function() {
-	console.log('\n... localhost listening on port 3000 ...\n');
+var connString = 'mongodb://admin:testing@ds061405.mongolab.com:61405/wvup_shows';
+
+MongoClient.connect(connString, function(err, db) {
+	if(err)
+		return console.log(err);
+	console.log("... connected to database ...");
+
+	app.listen(3000, function() {
+		console.log('... the magic happens on port 3000 ...\n');
+	});
+
+	_db = db;
 });
