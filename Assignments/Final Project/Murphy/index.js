c1f7ac3f12a3c7d72da6d7565661bf7d0c17205d
Casey Murphy <st01k@lvaas.net>
2016-04-14 18:35:55 -0400

    fixed it

--- Assignments/Final Project/Murphy/index.js
+++ Assignments/Final Project/Murphy/index.js
@@ -26,3 +26,49 @@ mongoClient.connect(connString, function(err, db) {
 
 	_db = db;
 });
+
+// routes --------------------------------------------------------------------
+app.get('/preview', function (req, res) {
+
+	var collection = _db.collection('tvshows');
+
+	collection.find({preview: 'yes'}, function (err, cursor) {
+		if (err)
+			return res.send(err);
+		cursor.toArray(function(err, doc) {
+			if (err)
+				return res.send(err);
+			res.send(doc);
+		});
+	});
+});
+
+app.get('/popular', function (req, res) {
+
+	var collection = _db.collection('tvshows');
+
+	collection.find({popular: 'yes'}, function (err, cursor) {
+		if (err)
+			return res.send(err);
+		cursor.toArray(function(err, doc) {
+			if (err)
+				return res.send(err);
+			res.send(doc);
+		});
+	});
+})
+
+app.get('/recent', function (req, res) {
+
+	var collection = _db.collection('tvshows');
+
+	collection.find({recent: 'yes'}, function (err, cursor) {
+		if (err)
+			return res.send(err);
+		cursor.toArray(function(err, doc) {
+			if (err)
+				return res.send(err);
+			res.send(doc);
+		});
+	});
+})
